306945dd0c434564d53907d7cf4db7261320defb
Naman1000 <91007344+Naman1000@users.noreply.github.com>
2026-05-13 20:14:36 +0530

    Add routing with Home and About components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 
 import React from "react";
 import './../styles/App.css';
+import Home from './Home.jsx'
+import About from "./About.jsx"
+import Navigation from "./Navigation.jsx"
+
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+    <Navigation />
+    
+    <Routes >
+    
+    <Route path="/" element={<Home />}  />
+    <Route path="/about" element={<About /> }  />
+
+    </Routes >
     </div>
   )
 }
